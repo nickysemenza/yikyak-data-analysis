d12500c3d287c0ddb47ab221e14b6ab10efbbe3d
Nicky Semenza <14nicholasse@gmail.com>
2014-10-20 20:21:47 -0400

    added map and location input

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 import pyak as pk
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, session, request, redirect, url_for
 import json
 # initialization
 app = Flask(__name__)
 app.config.update(
     DEBUG = True,
+    SECRET_KEY = open("/dev/random","rb").read(32) 
 )
 
-# controllers
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
@@ -17,21 +17,34 @@ def favicon():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@app.route("/listing")
-def listing():
-	location = pk.Location(40.423705, -86.921195)
+@app.route("/map",methods=['GET', 'POST'])
+def map():
+	lat=request.args.get('latitude')
+	lon=request.args.get('longitude')
+	if(lon==None):
+		lon="-86.9241416"
+	if(lat==None):
+		lat="40.42569479"
+	print(lat)
+	location = pk.Location(lat, lon)
 	testyakker = pk.Yakker(None, location, False)
 	#yaklist = testyakker.get_area_tops()
 	yaks_new = []
+	words=[]
 	for yak in testyakker.get_yaks():
 	    yaks_new.append(yak.return_yak())
 	yaks_top = []
 	for yak in testyakker.get_area_tops():
 	    yaks_top.append(yak.return_yak())
+	    words += yak.return_yak().get('message').split()
 	yaks_greatest =[]
 	for yak in testyakker.get_greatest():
 	    yaks_greatest.append(yak.return_yak())
-	return render_template('listing.html',yaks_new=yaks_new,yaks_top=yaks_top,yaks_greatest=yaks_greatest)
+	data= {}
+	data['latitude']=lat
+	data['longitude']=lon
+	print json.dumps(words)
+	return render_template('map.html',yaks_new=yaks_new,yaks_top=yaks_top,yaks_greatest=yaks_greatest,data=data,words=words)
 
 @app.route("/")
 def index():
@@ -41,3 +54,5 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    session['latitude']="40.425694799999995"
+    session['longitude']="-86.9241416"
